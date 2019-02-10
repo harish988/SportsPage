f022a40c4b0e9ad7c1ed00f087684ec3cd266934
Kalathiappan <harishharish988@gmail.com>
2019-02-10 13:24:37 +0530

    Second18

--- sports/settings.py
+++ sports/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
 ALLOWED_HOSTS = ['mepcosport.herokuapp.com']
 
