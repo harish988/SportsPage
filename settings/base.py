9b9429014d2001d056e70b635b70f3ee0bdc549b
Kalathiappan <harishharish988@gmail.com>
2019-02-10 11:35:01 +0530

    Second2

--- settings/base.py
+++ settings/base.py
@@ -1,4 +1,4 @@
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
