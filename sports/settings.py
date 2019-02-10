c0f2ce84b37c7415ff7e2206eec76114f1cb7cab
Kalathiappan <harishharish988@gmail.com>
2019-02-10 13:04:26 +0530

    Second14

--- sports/settings.py
+++ sports/settings.py
@@ -130,5 +130,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'home/static'),
+    os.path.join(BASE_DIR, 'results/static'),
+    os.path.join(BASE_DIR, 'standings/static'),
 )
